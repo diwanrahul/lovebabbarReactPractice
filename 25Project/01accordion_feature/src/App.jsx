8dbd4a0491fed87f27879875b41649f2348c4284
diwanrahul <72813257+diwanrahul@users.noreply.github.com>
2024-02-29 14:24:42 +0530

    changes

--- 25Project/01accordion_feature/src/App.jsx
+++ 25Project/01accordion_feature/src/App.jsx
@@ -6,6 +6,11 @@ import StarRating from "./components/star-rating"
 import { LoadMoreData } from "./components/load-more-data"
 import TreeView from "./components/tree-view"
 import menus from "./components/tree-view/data"
+import QRCodeGenerator from "./components/QR-code-generator"
+import LightDarkTheme from "./components/light-dark-theme"
+import ScrollIndicator from "./components/scroll-indicator"
+import TabTest from "./components/custom-tabs/tab-test"
+import ModalTest from "./components/custom-modal-popup/modal-test"
 function App() {
   const url = "https://picsum.photos/v2/list";
 
@@ -27,7 +32,23 @@ function App() {
        {/* <LoadMoreData /> */}
 
        {/* Tree TreeView */}
-       <TreeView menus={menus}/>
+       {/* <TreeView menus={menus}/> */}
+
+       {/* QR code Generator */}
+       {/* <QRCodeGenerator /> */}
+
+       {/* light dark them */}
+       {/* <LightDarkTheme />  */}
+
+       {/* ScrollIndicator */}
+       {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/> */}
+
+       {/* custom tabs component */}
+       {/* <TabTest /> */}
+
+
+       {/* custom modal components */}
+       <ModalTest />
 
     </>
   )
