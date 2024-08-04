bf24a9abd13030699e37939438c8abfd46400904
diwanrahul419 <diwanrahul419@gmail.com>
2024-08-04 22:02:22 +0530

    added

--- 25Project/portfolio_website/frontend_portfolio/src/client.js
+++ 25Project/portfolio_website/frontend_portfolio/src/client.js
@@ -5,11 +5,11 @@ import { createClient } from '@sanity/client';
 console.log(import.meta.env); 
 
 export const client = createClient({
-    projectId: "65an1aw3",
+    projectId: "",
     dataset: "production",
     apiVersion: "2024-07-24",
     useCdn: false,
-    token: "skA4YhUHGkxyQIqaOcaplvKa6eHfJwfi3mIr3z3YuaH7gIvita2zN4Bq2s8MubBJBvYxbZUmUDsToPpwWFyYhJpyQFiu6XZ82SmaijFRvbmbZmdySDmS6XQxuHLJC5a5EWf9AZhNc0FlcYHXONlFJ6YObnOsIeDPPYL1izCFO6rKOzFF4bLQ",
+    token: "",
 });
 
 const builder = imageUrlBuilder(client);
